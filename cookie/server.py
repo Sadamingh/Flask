34debe5198095f72fcf143e3fe41632278e262ac
Sadamingh <yxing11@usfca.edu>
2020-11-26 22:23:55 +0800

    add files

--- cookie/server.py
+++ cookie/server.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template
+from flask import redirect, url_for
+from flask import Flask, render_template, request, make_response
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-   return render_template('index.html')
+   name = request.cookies.get('userID')
+   if name is not None:
+   		return redirect(url_for('getcookie'))
+   else:
+   		return render_template('index.html')
 
 
 @app.route('/setcookie', methods = ['POST', 'GET'])
 def setcookie():
    if request.method == 'POST':
-   user = request.form['nm']
-   
-   resp = make_response(render_template('readcookie.html'))
-   resp.set_cookie('userID', user)
+   		user = request.form['nm']
+
+   		resp = make_response(render_template('readcookie.html'))
+   		resp.set_cookie('userID', user)
    
    return resp
 
@@ -21,4 +28,19 @@ def setcookie():
 @app.route('/getcookie')
 def getcookie():
    name = request.cookies.get('userID')
-   return '<h1>welcome ' + name + '</h1>'
+
+   return render_template('welcomepage.html', name=name)
+
+
+@app.route('/removecookie', methods = ['POST', 'GET'])
+def removecookie():
+   if request.method == 'POST':
+   	   user = request.form['nm']
+
+   resp = make_response(render_template('index.html'))
+   resp.delete_cookie('userID')
+
+   return resp
+
+
+app.run('0.0.0.0')
